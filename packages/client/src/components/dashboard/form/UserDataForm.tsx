47266ba63107d3e2ad4d1560cd89a316786585d8
meryam soussi <meryam.soussi@artelys.com>
2022-07-25 17:11:10 +0200

    Add tag input : dynamic input for adding or removing tags

--- packages/client/src/components/dashboard/form/UserDataForm.tsx
+++ packages/client/src/components/dashboard/form/UserDataForm.tsx
@@ -1,13 +1,53 @@
-import { Component } from 'react'
-import { Col, Input, Row } from 'antd';
+import React, { Component } from 'react'
+import { Button, Col, Input, Row, Tag, Tooltip } from 'antd';
 import { UserOutlined, TagOutlined, EditFilled } from '@ant-design/icons';
 
-// To send the user information to ViewSetup
-// INPUT: 
-// Output: title, author, tags
+export default class UserDataForm extends Component<any, any> {
 
-export default class UserDataForm extends Component {
+    saveInputRef;
+    constructor(props) {
+        super(props)
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleInputConfirm = this.handleInputConfirm.bind(this);
+        this.saveInputRef = React.createRef();
+        this.state = {
+            tags: ['Tag 1', 'Tag 2'],
+            inputVisible: false,
+            inputValue: '',
+        };
+    }
+
+    showInput = () => {
+        this.setState({ inputVisible: true }, () => this.saveInputRef.current.focus());
+    }
+
+    handleInputChange = (e) => {
+        this.setState({ inputValue: e.target.value });
+    }
+
+    handleInputConfirm = () => {
+        const state = this.state;
+        const inputValue = state.inputValue;
+        let tags = state.tags;
+        if (inputValue && tags.indexOf(inputValue) === -1) {
+            tags = [...tags, inputValue];
+        }
+        this.setState({
+            tags,
+            inputVisible: false,
+            inputValue: '',
+        });
+    }
+
+    handleClose = (removedTag) => {
+        const tags = this.state.tags.filter(tag => tag !== removedTag);
+        console.log(tags);
+        this.setState({ tags });
+    }
+
     render() {
+        const { tags, inputVisible, inputValue } = this.state;
+
         return (
             <Row justify="space-evenly">
                 <Col xs={20} sm={20} md={6} lg={7} >
@@ -24,11 +64,34 @@ export default class UserDataForm extends Component {
                 </Col>
 
                 <Col xs={20} sm={20} md={6} lg={7} >
-                    {/* Search for antd design tag */}
-                    <Input
-                        prefix={<TagOutlined className="site-form-item-icon" />}
-                        placeholder="Tag1; Tag2; "
-                    />
+
+                    <div className='tag-input-content'>
+                        <TagOutlined className="site-form-item-icon" />
+                        <p>
+                            {tags.map((tag, index) => {
+                                const isLongTag = tag.length > 20;
+                                const tagElem = (
+                                    <Tag key={tag} closable onClose={() => this.handleClose(tag)}>
+                                        {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                                    </Tag>
+                                );
+                                return isLongTag ? <Tooltip title={tag} key={tag}>{tagElem}</Tooltip> : tagElem;
+                            })}
+
+                            {inputVisible && (
+                                <Input
+                                    ref={this.saveInputRef}
+                                    size="small"
+                                    className="tag-input"
+                                    value={inputValue}
+                                    onChange={this.handleInputChange}
+                                    onBlur={this.handleInputConfirm}
+                                    onPressEnter={this.handleInputConfirm}
+                                />
+                            )}
+                            {!inputVisible && <Button size="small" type="dashed" onClick={this.showInput}>+ New Tag</Button>}
+                        </p>
+                    </div>
                 </Col>
             </Row>
         )
