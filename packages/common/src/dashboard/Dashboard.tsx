0d0de9360aa4800d3549961f87209265fd910f9c
meryam soussi <meryam.soussi@artelys.com>
2022-07-27 11:07:18 +0200

    Fix the drawer placment (right and left)

--- packages/common/src/dashboard/Dashboard.tsx
+++ packages/common/src/dashboard/Dashboard.tsx
@@ -11,77 +11,29 @@ import { Button, Drawer, Space } from 'antd';
 
 export default class Dashboard extends Component<any, any> {
 
-  sidebarRef;
-
   constructor(props) {
     super(props);
     this.state = {
       collapsed: false,
-      placement:'right',
-      isResizing: false,
-      sidebarWidth: (window.innerWidth) * 0.3,
-      contentWidth: window.innerWidth - (window.innerWidth) * 0.3
-    }
-    this.sidebarRef = React.createRef();
-
-    console.log("Dashboard: ", this.props);
-  }
-  startResizing = () => {
-    this.setState({ isResizing: true });
-  };
-  stopResizing = () => {
-    this.setState({ isResizing: false });
-  };
-
-  resize = (mouseMoveEvent) => {
-    if (this.state.isResizing) {
-      const sidebarWidth = mouseMoveEvent.clientX -
-        this.sidebarRef.current.getBoundingClientRect().left;
-      const contentWidth = window.innerWidth - sidebarWidth;
-      this.setState({
-        sidebarWidth, contentWidth
-      }, () => {
-        window.dispatchEvent(new Event('resize'));
-      })
+      placement: 'left',
     }
   }
 
   componentDidMount() {
-    window.addEventListener("mousemove", this.resize);
-    window.addEventListener("mouseup", this.stopResizing);
     window.scrollTo(0, 0)
-
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("mousemove", this.resize);
-    window.removeEventListener("mouseup", this.stopResizing);
   }
 
   render() {
-    const setCollapsed = () => {
-      const state = {
-        sidebarWidth: (window.innerWidth) * 0.3,
-        contentWidth: window.innerWidth - (window.innerWidth) * 0.3,
-        collapsed: !this.state.collapsed
-      }
-      if (state.collapsed) state.contentWidth = window.innerWidth
-      else state.contentWidth = window.innerWidth - state.sidebarWidth
-
-      this.setState(state, () => {
-        window.dispatchEvent(new Event('resize'));
-      })
-    }
-
     const setVisibility = () => {
-      this.setState({        collapsed: !this.state.collapsed
+      this.setState({
+        collapsed: !this.state.collapsed
       })
     }
 
-    const setPlacement = (e) =>{
-      console.log("target: ",e.target)
-      this.setState({placement: e.target.value})
+    const setPlacement = (e) => {
+      this.setState({ placement: e.currentTarget.value })
     }
+
     return (
       <div className='dashboard'>
         <div>
@@ -92,19 +44,19 @@ export default class Dashboard extends Component<any, any> {
             onClose={setVisibility}
             maskClosable={false}
             mask={false}
+            className={"drawer"}
+            style={!this.state.collapsed ? {zIndex:'-1'}: {zIndex:'0'}}
             extra={
               <Space>
                 <Button onClick={setPlacement} value="left">left</Button>
-                <Button type="primary" onClick={setPlacement} value="right">
-                  right
-                </Button>
+                <Button onClick={setPlacement} value="right">right</Button>
               </Space>
             }
           >
             <DashboardConfigControl data={this.props} />
           </Drawer>
         </div>
-        <div className="dashboard-content" style={{width: "100%"}}>
+        <div className="dashboard-content">
           <div>
             {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
               className: 'trigger',
