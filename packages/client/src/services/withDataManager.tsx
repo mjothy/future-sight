73db2bd0abf3fa44386479adae4340d798fad7f2
meryam soussi <meryam.soussi@artelys.com>
2022-07-28 17:30:29 +0200

    Add displayName to HOC(withDataManager)

--- packages/client/src/services/withDataManager.tsx
+++ packages/client/src/services/withDataManager.tsx
@@ -4,9 +4,14 @@ import DataManager from './DataManager';
 const dataManager = new DataManager();
 
 export default function withDataManager(Component) {
-    return class extends React.Component {
+    class withDataManager extends React.Component {
+        static displayName: string;
         render(){
             return <Component {...this.props} dataManager = {dataManager} />
         }
     }
+
+    const name = Component.displayName || Component.name;
+    withDataManager.displayName = `withDataManager(${name})`;
+    return withDataManager;
 }
