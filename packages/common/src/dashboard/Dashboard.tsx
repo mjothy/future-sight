7c8073b8ca0be45dd75882518b037e28abeb91fe
meryam soussi <meryam.soussi@artelys.com>
2022-08-12 17:47:01 +0200

    Sidebar placement

--- packages/common/src/dashboard/Dashboard.tsx
+++ packages/common/src/dashboard/Dashboard.tsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import DashboardConfigView from './DashboardConfigView';
 import DashboardConfigControl from './DashboardConfigControl';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined
+  MenuFoldOutlined
 } from '@ant-design/icons';
 import Sidebar from './sidebar/Sidebar';
 
@@ -13,6 +12,7 @@ export default class Dashboard extends Component<any, any> {
     super(props);
     this.state = {
       sidebarVisible: false,
+      placement: 'right'
     }
   }
 
@@ -35,16 +35,20 @@ export default class Dashboard extends Component<any, any> {
       })
     }
 
+    const setPlacement = (e) => {
+      this.setState({ placement: e.currentTarget.value })
+  }
+
     return (
 
       <div className='dashboard'>
-        <Sidebar sidebarVisible={this.state.sidebarVisible} setVisibility={setVisibility} {...this.props} >
+        <Sidebar sidebarVisible={this.state.sidebarVisible} placement={this.state.placement} setVisibility={setVisibility} setPlacement = {setPlacement} {...this.props} >
           <DashboardConfigControl {...this.props} />
         </Sidebar>
         <div className="dashboard-content">
           <div>
-            {React.createElement(this.state.sidebarVisible ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
+            { !this.state.sidebarVisible && React.createElement(MenuFoldOutlined, {
+              className: 'sidebar-'+this.state.placement,
               onClick: () => setVisibility(),
             })}
           </div>
--- packages/common/src/dashboard/sidebar/Sidebar.tsx
+++ packages/common/src/dashboard/sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import {
-    LeftCircleFilled
+    LeftCircleFilled,
+    MenuUnfoldOutlined,
+    PicLeftOutlined,
+    PicRightOutlined
 } from '@ant-design/icons';
 import { Button, Drawer, Space } from 'antd';
 export default class Sidebar extends Component<any, any> {
@@ -14,34 +17,30 @@ export default class Sidebar extends Component<any, any> {
 
     constructor(props) {
         super(props);
-        this.state = {
-            placement: 'right',
-        }
     }
 
     render() {
 
-        const setPlacement = (e) => {
-            this.setState({ placement: e.currentTarget.value })
-        }
-
         return (
             <Drawer
-                placement={this.state.placement}
+                placement={this.props.placement}
                 width={500}
                 visible={this.props.sidebarVisible}
                 onClose={this.props.setVisibility}
+                closeIcon={<MenuUnfoldOutlined />}
                 maskClosable={true}
                 mask={true}
                 className={"drawer"}
                 style={!this.props.sidebarVisible ? { zIndex: '-1' } : { zIndex: '999' }}
                 extra={
                     <Space>
-                        <Button onClick={() => this.props.submitSetupView(false)}>
-                            <LeftCircleFilled />
+                            <LeftCircleFilled onClick={() => this.props.submitSetupView(false)} />
+                        <Button onClick={this.props.setPlacement} value="left">
+                        <PicLeftOutlined />
                         </Button>
-                        <Button onClick={setPlacement} value="left">left</Button>
-                        <Button onClick={setPlacement} value="right">right</Button>
+                        <Button onClick={this.props.setPlacement} value="right">
+                        <PicRightOutlined />
+                        </Button>
                     </Space>
                 }
             >
