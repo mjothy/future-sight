97c3f92c2e5502dc76081cc85577121d83f7c55a
Shiny Saing <shiny.saing@artelys.com>
2022-09-04 19:27:55 +0200

    Start from draft

--- packages/client/src/components/home/HomeView.tsx
+++ packages/client/src/components/home/HomeView.tsx
@@ -5,12 +5,25 @@ import { Link } from 'react-router-dom';
 const HomeView: React.FC = () => {
   const [draftFromURL, setDraftFromURL] = useState('');
 
+  const getDraftsElement = () => {
+    if (localStorage.length > 1) {
+      return <Link to="drafts">Continue from a draft</Link>;
+    }
+    if (localStorage.length === 1) {
+      const url = `draft?id=${localStorage.key(0)}`;
+      return <Link to={url}>Continue from the last draft</Link>;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div className="container">
       <h2>Welcome to FutureSight!</h2>
       <Button type="primary">
         <Link to="draft">Create a new Dashboard</Link>
       </Button>
+      {getDraftsElement()}
       <Input.Group style={{ display: 'flex', flexDirection: 'row' }}>
         <Button type="primary" disabled={!draftFromURL}>
           Start from another Dashboard
