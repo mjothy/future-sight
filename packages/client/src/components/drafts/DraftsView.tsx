05b0267be47ac1f96ff2d41fc7584d49133ee452
Shiny Saing <shiny.saing@artelys.com>
2022-09-20 14:28:55 +0200

    Fix overflow issue that occurs when the user has too many drafts

--- packages/client/src/components/drafts/DraftsView.tsx
+++ packages/client/src/components/drafts/DraftsView.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {Col, Image, Row} from 'antd';
 import './DraftsView.css';
-import Preview from "../Preview";
-import {getDrafts} from "./DraftUtils";
-import PreviewGroup from "../PreviewGroup";
+import Preview from '../Preview';
+import { getDrafts } from './DraftUtils';
 
 export default class DraftsView extends React.Component<any, any> {
-    objectMap = (obj, fn) => {
-        const ret = Object.fromEntries(
-            Object.entries(obj).map(
-                ([k, v], i) => [k, fn(k, v, i)]
-            )
-        );
-        return Object.values(ret);
-    }
+  objectMap = (obj, fn) => {
+    const ret = Object.fromEntries(
+      Object.entries(obj).map(([k, v], i) => [k, fn(k, v, i)])
+    );
+    return Object.values(ret);
+  };
 
-    render() {
-        return (
-            <div className="drafts-view-wrapper">
-                <h2>Your drafts</h2>
-                <div className="drafts-container">
-                    <div className="drafts-wrapper">
-                        <PreviewGroup dashboards={getDrafts()} urlPrefix={'/draft?id='} />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="drafts-view-wrapper">
+        <h2 style={{ marginTop: '0.5em' }}>Your drafts</h2>
+        <div className="drafts-container">
+          {Object.entries(getDrafts()).map(([key, value]) => {
+            return (
+              <div key={key} className="draft-preview">
+                <Preview id={key} conf={value} urlPrefix={'/draft?id='} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
