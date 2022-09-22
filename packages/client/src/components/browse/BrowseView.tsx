9bd16e4e87177667ceb99dfd6b51ad538eb0a341
Shiny Saing <shiny.saing@artelys.com>
2022-09-22 18:33:50 +0200

    WIP browse

--- packages/client/src/components/browse/BrowseView.tsx
+++ packages/client/src/components/browse/BrowseView.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import { Form, Input, Col, Row, Spin } from 'antd';
+import { Form, Input, Col, Row, Spin, Button, Alert } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import './BrowseView.css';
 
 export default class DraftsView extends React.Component<any, any> {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+      dashboards: [],
+      submitDisabled: true,
+    };
   }
+
+  componentDidMount(): void {
+    this.fetchInitData();
+  }
+
+  fetchInitData = () => {
+    return 'test';
+  };
+
+  /**
+   * Check that a least one field is defined
+   * @param values the form's values
+   * @returns whether there's a field that is defined or not
+   */
+  checkValues = (values) => {
+    return Object.values(values).find((val) => !!val);
+  };
+
+  onFormValuesChange = (values) => {
+    if (this.checkValues(values)) {
+      this.setState({ submitDisabled: false });
+    } else {
+      this.setState({ submitDisabled: true });
+    }
+  };
+
   render() {
+    const { dashboards, loading, submitDisabled } = this.state;
     return (
       <div className="browse-container">
         <h2>Browse a dashboard</h2>
-        <div>
-          <Form className="form">
+        <div className="form-container">
+          <Form className="form" onValuesChange={this.onFormValuesChange}>
             <Row justify="space-around" align="middle">
               <Col span={7}>
                 <Form.Item name="title" label="Title">
@@ -41,9 +74,30 @@ export default class DraftsView extends React.Component<any, any> {
                 </Form.Item>
               </Col>
             </Row>
+            <div className="submit-container">
+              {submitDisabled && (
+                <Alert
+                  message="You need to fill at least one input to browse a dashboard"
+                  type="warning"
+                  style={{ marginBottom: 10 }}
+                />
+              )}
+              <Button
+                type="primary"
+                htmlType="submit"
+                icon={<SearchOutlined />}
+                disabled={submitDisabled}
+              >
+                Browse
+              </Button>
+            </div>
           </Form>
           <div className="search-result">
-            <Spin />
+            {loading && <Spin />}
+            {dashboards.length > 0 &&
+              dashboards.map((e) => {
+                e;
+              })}
           </div>
         </div>
       </div>
