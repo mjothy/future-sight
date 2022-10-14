c872e491007f0976ef03a8493f01ed13d13197f4
meryam soussi <meryam.soussi@artelys.com>
2022-10-14 11:46:17 +0200

    Refactoring ControlBlockView

--- packages/common/src/dashboard/blocks/control/ControlBlockView.tsx
+++ packages/common/src/dashboard/blocks/control/ControlBlockView.tsx
@@ -1,142 +1,78 @@
 import { Col, Row, Select } from 'antd';
 import { Component } from 'react';
-import ControlBlockTableSelection from '../component/ControlBlockTableSelection';
-import BlockStyleModel from "../../../models/BlockStyleModel";
-import { CheckCircleOutlined, ClearOutlined } from '@ant-design/icons';
+import BlockStyleModel from '../../../models/BlockStyleModel';
 
 const { Option } = Select;
 
 export default class ControlBlockView extends Component<any, any> {
-  render() {
-    const { currentBlock } = this.props;
-    const metaData = currentBlock.config.metaData;
-
-    const variablesSelectionChange = (selectedVariables: string[]) => {
-      // Update the controlBlock data
-      metaData.master['variables'].values = selectedVariables;
-      this.props.updateBlockMetaData(
-        { master: metaData.master },
-        this.props.currentBlock.id
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: Object.keys(this.props.filters),
     };
+  }
 
-    const modelsSelectionChange = (selectedModels: string[]) => {
-      // Update the controlBlock data
-      metaData.master['models'].values = selectedModels;
-      this.props.updateBlockMetaData(
-        { master: metaData.master },
-        this.props.currentBlock.id
-      );
-    };
+  onChange = (option, selectedData: string[]) => {
+    const metaData = this.props.currentBlock.config.metaData;
+    metaData.master[option].values = selectedData;
+    this.props.updateBlockMetaData(
+      { master: metaData.master },
+      this.props.currentBlock.id
+    );
 
-    const scenariosSelectionChange = (selectedScenarios: string[]) => {
-      // Update the controlBlock data
-      metaData.master['scenarios'].values = selectedScenarios;
-      this.props.updateBlockMetaData(
-        { master: metaData.master },
-        this.props.currentBlock.id
-      );
-    };
+    // Update also children
+  };
 
-    const regionsSelectionChange = (selectedRegions: string[]) => {
-      metaData.master['regions'].values = selectedRegions;
-      this.props.updateBlockMetaData(
-        { master: metaData.master },
-        this.props.currentBlock.id
-      );
-    };
-    const configStyle: BlockStyleModel = this.props.currentBlock.config.configStyle;
+  selectDropDown = (option) => {
+    const metaData = this.props.currentBlock.config.metaData;
     return (
-      <div style={{ maxHeight: this.props.height - 30, overflowY: "auto", paddingRight: "10px", paddingLeft: "10px" }}>
+      <Row className="mb-10">
+        <Col span={24}>
+          <Select
+            mode="multiple"
+            className="width-100"
+            placeholder={option}
+            defaultValue={metaData.master[option].values}
+            onChange={(e) => this.onChange(option, e)}
+          >
+            {metaData[option].map((element) => (
+              <Option key={element} value={element}>
+                {element}
+              </Option>
+            ))}
+          </Select>
+        </Col>
+      </Row>
+    );
+  };
+
+  render() {
+    const metaData = this.props.currentBlock.config.metaData;
+
+    const configStyle: BlockStyleModel =
+      this.props.currentBlock.config.configStyle;
+    return (
+      <div
+        style={{
+          maxHeight: this.props.height - 30,
+          overflowY: 'auto',
+          paddingRight: '10px',
+          paddingLeft: '10px',
+        }}
+      >
         {configStyle.title.isVisible ? (
           <Row>
             <Col span={24}>
               <h3>{configStyle.title.value}</h3>
             </Col>
           </Row>
-        ) : undefined
-        }
+        ) : undefined}
 
-        {metaData.master['models'].isMaster && (
-          <Row className="mb-10">
-            <Col span={24}>
-              <Select
-                mode="multiple"
-                className="width-100"
-                placeholder="Models"
-                defaultValue={metaData.master['models'].values}
-                onChange={modelsSelectionChange}
-              >
-                {metaData.models.map((model) => (
-                  <Option key={model} value={model}>
-                    {model}
-                  </Option>
-                ))}
-              </Select>
-            </Col>
-          </Row>
-        )}
-
-        {metaData.master['scenarios'].isMaster && (
-          <Row className="mb-10">
-            <Col span={24}>
-              <Select
-                mode="multiple"
-                className="width-100"
-                placeholder="Scenarios"
-                defaultValue={metaData.master['scenarios'].values}
-                onChange={scenariosSelectionChange}
-              >
-                {metaData.scenarios.map((scenario) => (
-                  <Option key={scenario} value={scenario}>
-                    {scenario}
-                  </Option>
-                ))}
-              </Select>
-            </Col>
-          </Row>
-        )}
-
-        {metaData.master['variables'].isMaster && (
-          <Row className="mb-10">
-            <Col span={24}>
-              <Select
-                mode="multiple"
-                className="width-100"
-                placeholder="Variables"
-                defaultValue={metaData.master['variables'].values}
-                onChange={variablesSelectionChange}
-              >
-                {metaData.variables.map((variable) => (
-                  <Option key={variable} value={variable}>
-                    {variable}
-                  </Option>
-                ))}
-              </Select>
-            </Col>
-          </Row>
-        )}
-
-        {metaData.master['regions'].isMaster && (
-          <Row className="mb-10">
-            <Col span={24} className={'checkbox-col-label'}>
-              <Select
-                // key={this.defaultRegions.toString()}
-                mode="multiple"
-                className="width-100"
-                placeholder="Regions"
-                defaultValue={metaData.master['regions'].values}
-                onChange={regionsSelectionChange}
-              >
-                {metaData.regions.map((region) => (
-                  <Option key={region} value={region}>
-                    {region}
-                  </Option>
-                ))}
-              </Select>
-            </Col>
-          </Row>
-        )}
+        {Object.keys(metaData.master).map((option) => {
+          if (metaData.master[option].isMaster) {
+            return this.selectDropDown(option);
+          }
+        })}
       </div>
     );
   }
