b19e49902bb8c39f748a4cba8b9709dbfbca7eaf
Ahmerique <ahmouckerick@gmail.com>
2022-10-28 10:55:14 +0200

    fix: controlled data block doesn't load variables and regions

--- packages/common/src/dashboard/blocks/data/DataBlockEditor.tsx
+++ packages/common/src/dashboard/blocks/data/DataBlockEditor.tsx
@@ -1,172 +1,161 @@
-import { Component } from 'react';
-import { Select, Space } from 'antd';
+import {Component} from 'react';
+import {Select, Space} from 'antd';
 import DataBlockTableSelection from '../component/DataBlockTableSelection';
-import BlockModel from '../../../models/BlockModel';
-import ConfigurationModel from '../../../models/ConfigurationModel';
 
-const { Option } = Select;
+//TODO variable selection do not change because currentBlock doesn't change
+const {Option} = Select;
 
 /**
  * The form in sidebar to add/edit dara block
  */
 export default class DataBlockEditor extends Component<any, any> {
-  variables: string[] = [];
-  regions: string[] = [];
-  defaultVariables: string[] = [];
-  defaultRegions: string[] = [];
-  isBlockControlled = false;
-  controlBlock: BlockModel = new BlockModel();
 
-  constructor(props) {
-    super(props);
-    this.updateDropdownData();
-    this.checkIfBlockControlled();
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    // the second condition to not update the dropdown list of ControlData
-    if (
-      prevProps.blockSelectedId !== this.props.blockSelectedId &&
-      this.props.currentBlock.blockType === 'data'
-    ) {
-      this.updateDropdownData();
-      this.checkIfBlockControlled();
+    constructor(props) {
+        super(props);
+        this.state = {
+            variables: [],
+            regions: [],
+        }
     }
-  }
 
-  /**
-   * To disable inputes that are controlled by ControlBlock
-   */
-  checkIfBlockControlled = () => {
-    const controlBlockId = this.props.currentBlock.controlBlock;
-    if (controlBlockId !== '') {
-      this.isBlockControlled = true;
-      this.controlBlock = this.props.blocks[controlBlockId];
-    } else {
-      this.isBlockControlled = false;
-      this.controlBlock = new BlockModel();
+    componentDidMount() {
+        this.updateDropdownData();
     }
-  };
 
-  /**
-   * Update the options of dropdown lists of variables and regions
-   * SHOW only the options when we can find data to visualize
-   */
-  updateDropdownData = () => {
-    const metaData = this.props.currentBlock.config.metaData;
-    // All data received from SetUp view
-    const dataStructure = this.props.dashboard.dataStructure;
-    this.variables = [];
-    this.regions = [];
-    // To show only variables and regions of selected data
-    Object.keys(metaData.models).map((modelKey) => {
-      metaData.models[modelKey].map((scenarioKey) => {
-        this.variables = [
-          ...this.variables,
-          ...dataStructure[modelKey][scenarioKey].variables,
-        ];
-        this.regions = [
-          ...this.regions,
-          ...dataStructure[modelKey][scenarioKey].regions,
-        ];
-      });
-    });
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        // the second condition to not update the dropdown list of ControlData
+        if (
+            prevProps.blockSelectedId !== this.props.blockSelectedId &&
+            this.props.currentBlock.blockType === 'data'
+        ) {
+            this.updateDropdownData();
+        }
+    }
 
-    // Show unique values in dropdown list options
-    this.variables = [...new Set(this.variables)];
-    this.regions = [...new Set(this.regions)];
+    /**
+     * Update the options of dropdown lists of variables and regions
+     * SHOW only the options when we can find data to visualize
+     */
+    updateDropdownData = () => {
+        const dataStructure = this.props.dashboard.dataStructure;
+        let variables: string[] = [];
+        let regions: string[] = [];
+        const metaData = this.props.currentBlock.config.metaData;
 
-    // Show selected/default values (check if the selected values exist in the dropdown list options)
-    this.defaultVariables = metaData.variables
-      .filter((variable: string) => this.variables.indexOf(variable) >= 0)
-      .map((variable) => variable);
-    this.defaultRegions = metaData.regions
-      .filter((region: string) => this.regions.indexOf(region) >= 0)
-      .map((region) => region);
+        // Not controlled block allow selection of only selected model
+        if (!this.props.currentBlock.controlBlock) {
+            Object.keys(metaData.models).map((modelKey) => {
+                metaData.models[modelKey].map((scenarioKey) => {
+                    variables.push(...dataStructure[modelKey][scenarioKey].variables);
+                    regions.push(...dataStructure[modelKey][scenarioKey].regions);
+                });
+            });
+        //  Controlled block allow selection of regions and variables of all model from control block        } else {
+        } else {
+            const controlBlockMetaData = this.props.blocks[this.props.currentBlock.controlBlock].config.metaData
+            Object.keys(controlBlockMetaData.models).map((modelKey) => {
+                controlBlockMetaData.models[modelKey].map((scenarioKey) => {
+                    variables.push(...dataStructure[modelKey][scenarioKey].variables);
+                    regions.push(...dataStructure[modelKey][scenarioKey].regions);
+                });
+            });
+        }
+        // Show unique values in dropdown list options
+        variables = [...new Set(variables)];
+        regions = [...new Set(regions)];
 
-    this.props.updateBlockMetaData({
-      variables: this.defaultVariables,
-      regions: this.defaultRegions,
-    });
-  };
+        this.setState({
+            variables,
+            regions
+        })
 
-  variablesSelectionChange = (selectedVariables: string[]) => {
-    this.props.updateBlockMetaData({ variables: selectedVariables });
-    this.updateDropdownData();
-  };
+        // Show selected/default values (check if the selected values exist in the dropdown list options)
+        const defaultVariables = metaData.variables
+            .filter((variable: string) => variables.indexOf(variable) >= 0);
+        const defaultRegions = metaData.regions
+            .filter((region: string) => regions.indexOf(region) >= 0);
 
-  regionsSelectionChange = (selectedRegions: string[]) => {
-    this.props.updateBlockMetaData({ regions: selectedRegions });
-    this.updateDropdownData();
-  };
+        this.props.updateBlockMetaData({
+            variables: defaultVariables,
+            regions: defaultRegions,
+        });
 
-  render() {
-    const metaData = this.props.currentBlock.config.metaData;
+    };
 
-    return (
-      <Space direction="vertical" className="width-100">
-        <div>
-          <h4>Model & Scenario selection</h4>
-          {this.isBlockControlled &&
-          (this.controlBlock.config as ConfigurationModel).metaData.master[
-              'models'
-              ].isMaster ? (
-              <p>That block is controled by Model/scenario</p>
-          ) : (
-              <DataBlockTableSelection
-                  {...this.props}
-                  updateDropdownData={this.updateDropdownData}
-              />
-          )}
-        </div>
-        {/* adding key, because react not updating the default value on state change */}
-        <div>
-        <h4>Variables selection</h4>
-          <Select
-            key={this.defaultVariables.toString()}
-            mode="multiple"
-            className="width-100"
-            placeholder="Variables"
-            defaultValue={metaData.variables}
-            onChange={this.variablesSelectionChange}
-            disabled={
-              this.isBlockControlled &&
-              (this.controlBlock.config as ConfigurationModel).metaData.master[
-                'variables'
-              ].isMaster
-            }
-          >
-            {this.variables.map((variable) => (
-              <Option key={variable} value={variable}>
-                {variable}
-              </Option>
-            ))}
-          </Select>
-        </div>
-        <div>
-          <h4>Regions selection</h4>
-          <Select
-            key={this.defaultRegions.toString()}
-            mode="multiple"
-            className="width-100"
-            placeholder="Regions"
-            defaultValue={metaData.regions}
-            onChange={this.regionsSelectionChange}
-            disabled={
-              this.isBlockControlled &&
-              (this.controlBlock.config as ConfigurationModel).metaData.master[
-                'regions'
-              ].isMaster
-            }
-          >
-            {this.regions.map((region) => (
-              <Option key={region} value={region}>
-                {region}
-              </Option>
-            ))}
-          </Select>
-        </div>
-      </Space>
-    );
-  }
+    variablesSelectionChange = (selectedVariables: string[]) => {
+        this.props.updateBlockMetaData({variables: selectedVariables});
+        this.updateDropdownData();
+    };
+
+    regionsSelectionChange = (selectedRegions: string[]) => {
+        this.props.updateBlockMetaData({regions: selectedRegions});
+        this.updateDropdownData();
+    };
+
+    inputIsMaster = (input) => {
+        const controlBlockId = this.props.currentBlock.controlBlock
+        console.log(controlBlockId)
+        console.log(!!controlBlockId)
+        return (
+            !!controlBlockId &&
+            this.props.blocks[controlBlockId].config.metaData.master[input].isMaster
+        )
+    }
+
+    render() {
+
+
+        return (
+            <Space direction="vertical" className="width-100">
+                <div>
+                    <h4>Model & Scenario selection</h4>
+                    {
+                        this.inputIsMaster("models")
+                        ? (<p>That block is controlled by Model/scenario</p>)
+                        : (
+                            <DataBlockTableSelection
+                                {...this.props}
+                                updateDropdownData={this.updateDropdownData}
+                            />
+                        )
+                    }
+                </div>
+                {/* adding key, because react not updating the defaults value on state change */}
+                <div>
+                    <h4>Variables selection</h4>
+                    <Select
+                        mode="multiple"
+                        className="width-100"
+                        placeholder="Variables"
+                        value={this.props.currentBlock.config.metaData.variables}
+                        onChange={this.variablesSelectionChange}
+                        disabled={this.inputIsMaster('variables')}
+                    >
+                        {this.state.variables.map((variable) => (
+                            <Option key={variable} value={variable}>
+                                {variable}
+                            </Option>
+                        ))}
+                    </Select>
+                </div>
+                <div>
+                    <h4>Regions selection</h4>
+                    <Select
+                        mode="multiple"
+                        className="width-100"
+                        placeholder="Regions"
+                        value={this.props.currentBlock.config.metaData.regions}
+                        onChange={this.regionsSelectionChange}
+                        disabled={this.inputIsMaster('regions')}
+                    >
+                        {this.state.regions.map((region) => (
+                            <Option key={region} value={region}>
+                                {region}
+                            </Option>
+                        ))}
+                    </Select>
+                </div>
+            </Space>
+        );
+    }
 }
