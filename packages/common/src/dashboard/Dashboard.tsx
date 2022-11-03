a1f0a2afb1feb02cac74a597de000d8e78aa3f77
Ahmerique <ahmouckerick@gmail.com>
2022-11-03 17:31:00 +0100

    keep size of dashboard when closing sidebar

--- packages/common/src/dashboard/Dashboard.tsx
+++ packages/common/src/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import DashboardConfigView from './DashboardConfigView';
 import DashboardConfigControl from './DashboardConfigControl';
 import Sidebar from './sidebar/Sidebar';
@@ -11,50 +11,51 @@ import {Layout} from "antd";
 import {Content} from "antd/es/layout/layout";
 
 export interface DashboardProps extends ComponentPropsWithDataManager {
-  dashboard: DashboardModel;
-  addBlock: (blockType: string, masterBlockId?: string) => void;
-  blockSelectedId: string;
-  layout: LayoutModel[];
-  updateLayout: (layout: LayoutModel[]) => void;
-  blocks: { [id: string]: BlockModel };
-  updateSelectedBlock: (blockSelectedId: string) => void;
-  updateBlockMetaData: (data: any) => void;
-  updateBlockStyleConfig: (data: any) => void;
-  saveDashboard: (callback: (idPermanent) => void, image?: string) => void;
-  isEmbedded?: boolean;
+    dashboard: DashboardModel;
+    addBlock: (blockType: string, masterBlockId?: string) => void;
+    blockSelectedId: string;
+    layout: LayoutModel[];
+    updateLayout: (layout: LayoutModel[]) => void;
+    blocks: { [id: string]: BlockModel };
+    updateSelectedBlock: (blockSelectedId: string) => void;
+    updateBlockMetaData: (data: any) => void;
+    updateBlockStyleConfig: (data: any) => void;
+    saveDashboard: (callback: (idPermanent) => void, image?: string) => void;
+    isEmbedded?: boolean;
 }
 
 export default class Dashboard extends Component<DashboardProps, any> {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
 
-  render() {
-    return (
-      <Layout
-        className="dashboard"
-        style={{ height: this.props.isEmbedded ? '100%' : undefined }}
-      >
-        {/*<Content className="dashboard-content">*/}
-        {/*  <DashboardConfigView {...this.props} />*/}
-        {/*</Content>*/}
-          <Content className={"dashboard-content-wrapper"}>
-              <div className="dashboard-content">
-                  <DashboardConfigView {...this.props} />
-              </div>
-          </Content>
-
-          <Sidebar
-            onClose={() => this.props.updateSelectedBlock('')}
-            {...this.props}
-        >
-          <DashboardConfigControl {...this.props} />
-        </Sidebar>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout
+                className="dashboard"
+                style={{height: this.props.isEmbedded ? '100%' : undefined}}
+            >
+                {/*<Content className="dashboard-content">*/}
+                {/*  <DashboardConfigView {...this.props} />*/}
+                {/*</Content>*/}
+                <div className="no-sidebar-margin"/>
+                <Content className={"dashboard-content-wrapper"}>
+                    <div className="dashboard-content">
+                        <DashboardConfigView {...this.props} />
+                    </div>
+                </Content>
+                <div className="no-sidebar-margin"/>
+                <Sidebar
+                    onClose={() => this.props.updateSelectedBlock('')}
+                    {...this.props}
+                >
+                    <DashboardConfigControl {...this.props} />
+                </Sidebar>
+            </Layout>
+        );
+    }
 }
