0efc9db72443393893c989299b86d26be0670d6d
meryam soussi <meryam.soussi@artelys.com>
2024-02-14 11:36:50 +0100

    Disable auth

--- packages/server/src/data_backend/IIASADataManager.ts
+++ packages/server/src/data_backend/IIASADataManager.ts
@@ -24,7 +24,7 @@ export default class IIASADataManager {
         const options = {
             method: method,
             headers: {
-                'Authorization': 'Bearer ' + process.env["access_token"],
+                // 'Authorization': 'Bearer ' + process.env["access_token"], // TODO
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(body)
